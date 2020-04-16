52f9081c9c703b9cfa7350b2d5fcfda95418938c
Luann <luann.bertaud@epitech.eu>
2020-04-16 12:29:42 +0200

    resolving collisions

--- src/scene/intro/update_intro_screen.c
+++ src/scene/intro/update_intro_screen.c
@@ -14,7 +14,9 @@ int update_intro_screen(game_t *game, intro_screen_t *screen, long int delta)
     screen->delta += delta;
     update_player(game->player, delta);
     if (move_player(game->player, screen->npc, delta)) {
+        block_move_on_collision(game->player, screen->npc, delta);
         sfView_setCenter(game->camera, get_player_position(game->player));
         sfRenderWindow_setView(game->window, game->camera);
     }
+    block_move_on_collision(game->player, screen->npc, delta);
 }
