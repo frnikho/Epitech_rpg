f4e7556b0a16a36b5799d5fe0822dfa0288f2bd8
Luann <luann.bertaud@epitech.eu>
2020-04-29 17:46:23 +0200

    dongeon suite

--- src/scene/overworld/init_overworld.c
+++ src/scene/overworld/init_overworld.c
@@ -144,7 +144,7 @@ void init_maps(overworld_t *world)
     maps[2]->obs_nb = 0;
     maps[2]->tile_size = 20;
     maps[2]->offset = (sfVector2f){0, 0};
-    maps[2]->zoom = 1.5f;
+    maps[2]->zoom = 2.0f;
     init_interactions_boxes_indexs(maps[2], (int []){DUNGEON_OUT, 0});
     init_layers_id(maps[2], (int []){1, 0}, (int []){0}, (int []){2, 0});
 
@@ -163,7 +163,8 @@ static int init_world_map(game_t *game, overworld_t *world)
 int init_overworld(game_t *game, overworld_t *world)
 {
     world->state = create_state(100, game, 0);
-    world->current_map = 0;
+    world->current_map = 2;
     init_world_map(game, world);
+    set_player_position(game->player, (sfVector2f){1200, 2500});
     return (0);
 }
